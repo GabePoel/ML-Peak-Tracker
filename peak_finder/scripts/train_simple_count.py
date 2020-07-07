2bdf59518587bd16e629cf340214a4fd30b280a8
Gabriel <gpe@berkeley.edu>
2020-07-07 08:35:10 -0700

    More split model improvements.

--- peak_finder/scripts/train_simple_count.py
+++ peak_finder/scripts/train_simple_count.py
@@ -22,4 +22,4 @@ model = tf.keras.Sequential([
 
 root = os.getcwd()
 location = os.path.join(parent, 'models')
-tm.passive_class_train(name='simple_count', location=location, data_size=1090, scale=(0,1,1024), noise=True, epochs=2, model_design=model, steps=1, optimizer='adadelta')
+tm.passive_class_train(name='simple_count', location=location, data_size=1000, scale=(0,1,1024), noise=True, epochs=2, model_design=model, steps=1, optimizer='adadelta')
